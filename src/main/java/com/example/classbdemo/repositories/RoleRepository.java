f0dc858f5f848910a160139455be34f405899da1
mwizerwa77 <mwizerwa77@gmail.com>
2021-06-01 15:15:38 +0200

    implemented security

--- src/main/java/com/example/classbdemo/repositories/RoleRepository.java
+++ src/main/java/com/example/classbdemo/repositories/RoleRepository.java
@@ -1,11 +1,17 @@
 package com.example.classbdemo.repositories;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.classbdemo.enumerations.ERoleName;
 import com.example.classbdemo.model.Role;
+import com.example.classbdemo.model.User;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+	Optional<Role> findByName(ERoleName roleName);
 }
--- src/main/java/com/example/classbdemo/repositories/UserRepository.java
+++ src/main/java/com/example/classbdemo/repositories/UserRepository.java
@@ -13,4 +13,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByEmail(String usernameOrMobileOrEmail);
 
+	Optional<User> findByEmailOrUsernameOrMobile(String login, String login2, String login3);
+
+	boolean existsByEmail(String email);
+
+	boolean existsByMobile(String mobile);
+
 }
